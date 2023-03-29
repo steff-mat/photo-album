6697ae2b7e80c0e5ab9d482f0f9715ccdc47278c
Stefan Maties <maties.stefan44@gmail.com>
2023-03-29 22:09:25 +0200

    Add dark mode switch

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const body = document.getElementById('body');
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
-const imgs = document.getElementsByTagName('img');
+const imgs = document.getElementsByClassName('img');
+const switcher = document.getElementById('switcher');
+const light = document.getElementById('light');
+const dark = document.getElementById('dark');
 const snowWhite = '#F3F6FB';
 const charcoalBlack = '#212121';
+
+function day() {
+  body.style.backgroundColor = snowWhite;
+  body.style.color = snowWhite;
+  header.style.background = charcoalBlack;
+  footer.style.background = charcoalBlack;
+  light.style.backgroundColor = 'rgba(255, 255, 0, 0.7)';
+  dark.style.backgroundColor = 'gray';
+}
+
+function night() {
+  body.style.backgroundColor = charcoalBlack;
+  body.style.color = charcoalBlack;
+  header.style.background = snowWhite;
+  footer.style.background = snowWhite;
+  light.style.backgroundColor = 'gray';
+  dark.style.backgroundColor = 'rgba(0, 0, 255, 0.7)';
+}
 
 if (
   window.matchMedia &&
   window.matchMedia('(prefers-color-scheme: light)').matches
 ) {
-  body.style.backgroundColor = snowWhite;
-  body.style.color = snowWhite;
-  header.style.background = charcoalBlack;
-  footer.style.background = charcoalBlack;
+  day();
 } else if (
   window.matchMedia &&
   window.matchMedia('(prefers-color-scheme: dark)').matches
 ) {
-  body.style.backgroundColor = charcoalBlack;
-  body.style.color = charcoalBlack;
-  header.style.background = snowWhite;
-  footer.style.background = snowWhite;
+  night();
 }
 
 for (let img of imgs) {
@@ -30,3 +45,9 @@ for (let img of imgs) {
     });
   }
 }
+
+switcher.addEventListener('click', () => {
+  if (light.style.backgroundColor === 'rgba(255, 255, 0, 0.7)') {
+    night();
+  } else day();
+});
